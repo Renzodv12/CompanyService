b925c7673b72feb02d47aaf637f55c91c7f4b54f
Renzodv <renzodragotto50@gmail.com>
2025-08-06 22:59:05 -0300

    tasks

--- CompanyService/Core/Utils/CommentHelper.cs
+++ CompanyService/Core/Utils/CommentHelper.cs
@@ -1,35 +1,79 @@
-﻿using CompanyService.Core.Entities;
+﻿// CompanyService/Core/Utils/CommentHelper.cs
+using CompanyService.Core.Entities;
 using CompanyService.Core.Models.Task;
 
 namespace CompanyService.Core.Utils
 {
     public static class CommentHelper
     {
-        public static List<CommentDto> BuildNestedComments(
-            IEnumerable<TaskComment> allComments,
-            Guid? parentId = null)
+        public static List<CommentDto> BuildNestedComments(IEnumerable<TaskComment> comments)
         {
-            var comments = allComments
-                .Where(c => c.ParentCommentId == parentId)
+            if (comments == null || !comments.Any())
+                return new List<CommentDto>();
+
+            var commentList = comments.ToList();
+
+            // Primero, obtener todos los comentarios padre (sin ParentCommentId)
+            var parentComments = commentList
+                .Where(c => c.ParentCommentId == null)
                 .OrderBy(c => c.CreatedAt)
-                .Select(c => new CommentDto
-                {
-                    Id = c.Id.ToString(),
-                    Author = new AuthorDto
-                    {
-                        Id = c.AuthorId.ToString(),
-                        Name = c.AuthorName,
-                        Username = c.AuthorUsername,
-                        Avatar = c.AuthorAvatar
-                    },
-                    CreatedAt = c.CreatedAt,
-                    Content = c.Content,
-                    Comments = BuildNestedComments(allComments, c.Id) // Recursión para replies
-                })
                 .ToList();
 
-            return comments;
+            var result = new List<CommentDto>();
+
+            foreach (var parentComment in parentComments)
+            {
+                var commentDto = new CommentDto
+                {
+                    Id = parentComment.Id.ToString(),
+                    Author = new AuthorDto
+                    {
+                        Id = parentComment.AuthorId.ToString(),
+                        Name = parentComment.AuthorName,
+                        Username = parentComment.AuthorUsername,
+                        Avatar = parentComment.AuthorAvatar
+                    },
+                    CreatedAt = parentComment.CreatedAt,
+                    Content = parentComment.Content,
+                    Comments = GetReplies(parentComment.Id, commentList)
+                };
+
+                result.Add(commentDto);
+            }
+
+            return result;
+        }
+
+        private static List<CommentDto> GetReplies(Guid parentCommentId, List<TaskComment> allComments)
+        {
+            var replies = allComments
+                .Where(c => c.ParentCommentId == parentCommentId)
+                .OrderBy(c => c.CreatedAt)
+                .ToList();
+
+            var replyDtos = new List<CommentDto>();
+
+            foreach (var reply in replies)
+            {
+                var replyDto = new CommentDto
+                {
+                    Id = reply.Id.ToString(),
+                    Author = new AuthorDto
+                    {
+                        Id = reply.AuthorId.ToString(),
+                        Name = reply.AuthorName,
+                        Username = reply.AuthorUsername,
+                        Avatar = reply.AuthorAvatar
+                    },
+                    CreatedAt = reply.CreatedAt,
+                    Content = reply.Content,
+                    Comments = GetReplies(reply.Id, allComments) // Recursivo para replies de replies
+                };
+
+                replyDtos.Add(replyDto);
+            }
+
+            return replyDtos;
         }
     }
-
 }
